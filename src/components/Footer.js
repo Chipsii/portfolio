860b6ea76f4e2d0195450755446907b5db7602e4
Hasib <hasibfire24@gmail.com>
2023-08-03 17:54:27 +0600

    footer and spinny animation added and build image

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react"; 
+import Layout from "./Layout";
+import Link from "next/link";
+
+const Footer = () => {
+    return (
+        <footer className=" w-full border-t-2 border-solid border-dark font-medium text-lg first-letter:
+        ">
+            <Layout classname=" py-8 flex items-center justify-between ">
+                <span>{new Date().getFullYear()}&copy; All Rights Reserved.</span>
+                <div className="flex items-center">
+                Build With <span className="text-primary text-2x1 px-1">&#9825;</span>
+                by&nbsp;<Link href="https://spotitubemerge.nexentra.online" className=" underline underline-offset-2 
+                " target={"_blank"}>Chips</Link>
+                </div>
+                <Link href="https://spotitubemerge.nexentra.online" target={"_blank"} className="underline underline-offset-2 ">
+                Say hello</Link>
+            </Layout>
+        </footer>
+    )
+}
+
+export default Footer;
