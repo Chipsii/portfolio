6ae9af3c1f47c34af0da2e0202ea953e0c5e4388
Hasib <hasibfire24@gmail.com>
2023-08-04 20:10:19 +0600

    finished about page and added real resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
 Explore my latest projects and articles, showcasing my expertise in React.js and web development.
               </p>
               <div className=' flex items-center self-start mt-2'>
-                <Link href="/dummy.pdf" target={"_blank"}
+                <Link href="/resume.pdf" target={"_blank"}
                 className=' flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark '
                 download={true}
 
