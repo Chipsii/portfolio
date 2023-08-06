2f268cc3a4e428b2853278861045186c5b1a7e89
Hasib <hasibfire24@gmail.com>
2023-08-06 11:55:11 +0600

    fixed the apostopi problem

--- src/pages/about.js
+++ src/pages/about.js
@@ -46,16 +46,16 @@ const about =() => {
             <div className="grid w-full grid-cols-8 gap-16 ">
                 <div className="col-span-3 flex flex-col items-start justify-start">
                     <h2 className=" mb-4 text-lg font-bold uppercase text-dark/75">Biography</h2>
-                    <p className=" font-medium ">Hi, I'm Chips, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
+                    <p className=" font-medium ">Hi, I&apos;m Chips, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
 and user-centered digital experiences. With 2 years of experience in the field. I am always looking for 
-new and innovative ways to bring my client's visions to life.
+new and innovative ways to bring my client&apos;s visions to life.
                     </p>
 
-<p className=" my-4 font-medium ">I believe that design is about more than just making things look pretty – it's about solving problems and 
+<p className=" my-4 font-medium ">I believe that design is about more than just making things look pretty – it&apos;s about solving problems and 
 creating intuitive, enjoyable experiences for users. 
 </p>
 
-<p className=" font-medium ">Whether I'm working on a website, mobile app, or 
+<p className=" font-medium ">Whether I&apos;m working on a website, mobile app, or 
 other digital product, I bring my commitment to design excellence and user-centered thinking to 
 every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
 </p>
