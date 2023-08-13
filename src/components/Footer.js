171c835426ba8545773e12cae7b47aaee213ae29
Hasib <hasibfire24@gmail.com>
2023-08-13 19:54:09 +0600

    making the site responsive for mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,11 @@ const Footer = () => {
   return (
     <footer
       className=" w-full border-t-2 border-solid border-dark font-medium text-lg 
-      dark:text-light dark:border-light
+      dark:text-light dark:border-light sm:text-base
       ">
-      <Layout classname=" py-8 flex items-center justify-between ">
+      <Layout classname=" py-8 flex items-center justify-between lg:flex-col lg:py-6">
         <span>{new Date().getFullYear()}&copy; All Rights Reserved.</span>
-        <div className="flex items-center">
+        <div className="flex items-center lg:py-2">
           Build With <span className="text-primary dark:text-primaryDark text-2x1 px-1">&#9825;</span>
           by&nbsp;
           <Link
