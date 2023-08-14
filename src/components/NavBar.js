2c6c3b11bdc450aacce7303b621294ecd45d09f1
Hasib <hasibfire24@gmail.com>
2023-08-14 17:09:16 +0600

    fixed the hire me button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -70,7 +70,7 @@ const NavBar = () => {
   };
 
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative z-10 lg:px-16 md:px-12 sm:px-8 ">
+    <header className="w-full px-32 pt-10 font-medium flex items-center justify-between dark:text-light relative z-10 lg:px-16 md:px-12 sm:px-8 ">
       
       <button className="flex-col justify-center items-center hidden lg:flex" onClick={handleClick}>
         <span className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'} `}></span>
