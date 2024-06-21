df1b510e798c4901c406a4d9385dccd2343b045b
Chipsii <hasibfire24@gmail.com>
2024-06-21 21:16:52 +0600

    fix: update the resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
               </p>
               <div className=" flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/resume.pdf"
+                  href="/Hasib-Hossain.pdf"
                   target={"_blank"}
                   className=" flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
                   hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
